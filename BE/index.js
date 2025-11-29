c3fcfa9c324b8248757c8584f9fbe9eeec33fad0
Rajeev Rawat <rajeevrawat2100@email.com>
2025-11-29 19:52:27 +0530

    feat: add get all books API endpoint

--- BE/index.js
+++ BE/index.js
@@ -12,6 +12,27 @@ initializeDatabase()
 
 const PORT = 3001
 
+
+async function getAllBook(){
+    try {
+        const book = await Book.find()
+        return book
+    } catch (error) {
+        throw error
+    }
+}
+
+app.get('/books', async (req,res) => {
+    try {
+        const book = await getAllBook()
+        res.status(201).json({message: 'Book Data: ', data: book}) 
+    } catch (error) {
+        res.status(500).json({error: 'Failed to Fetch movie details'})
+    }
+})
+
+
+
 app.listen(PORT, () => {
     console.log('Server is running on this', PORT)
 })
