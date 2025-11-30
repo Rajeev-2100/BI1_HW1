8442f6fcdc5151c2086e5555b0da2f56ca2fbcbf
Rajeev Rawat <rajeevrawat2100@email.com>
2025-11-30 21:19:48 +0530

    feat: add delete book by ID route

--- BE/index.js
+++ BE/index.js
@@ -1,13 +1,13 @@
-const Book = require('./models/book.model.js')
-const express = require('express')
-const cors = require('cors')
+const Book = require("./models/book.model.js");
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-const fs = require('fs')
-app.use(express.json())
+const fs = require("fs");
+app.use(express.json());
 
-const { initializeDatabase } = require('./db/db.connect.js')
+const { initializeDatabase } = require("./db/db.connect.js");
 
 const corsOptions = {
   origin: "*",
@@ -17,93 +17,112 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+initializeDatabase();
 
-initializeDatabase()
-
-async function saveNewBook(createNewBook){
-    try {
-        const book = await Book(createNewBook)
-        return book.save()
-    } catch (error) {
-        throw error
-    }
+async function saveNewBook(createNewBook) {
+  try {
+    const book = await Book(createNewBook);
+    return book.save();
+  } catch (error) {
+    throw error;
+  }
 }
 
-app.post('/books', async (req,res) => {
-    try {
-        const book = await saveNewBook(req.body)
-        res.json(book)
-    } catch (error) {
-        console.error(error.message)
-        res.status(500).json({eror: "Failed to fetch book detail"})
-    }
-})
+app.post("/books", async (req, res) => {
+  try {
+    const book = await saveNewBook(req.body);
+    res.json(book);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ eror: "Failed to fetch book detail" });
+  }
+});
 
-async function getAllBook(){
-    try {
-        const book = await Book.find()
-        return book
-    } catch (error) {
-        throw error
-    }
+async function getAllBook() {
+  try {
+    const book = await Book.find();
+    return book;
+  } catch (error) {
+    throw error;
+  }
 }
 
-app.get('/books', async (req,res) => {
-    try {
-        const book = await getAllBook()
-        res.status(201).json({message: 'Book Data: ', data: book}) 
-    } catch (error) {
-        res.status(500).json({error: 'Failed to Fetch movie details'})
-    }
-})
+app.get("/books", async (req, res) => {
+  try {
+    const book = await getAllBook();
+    res.status(201).json({ message: "Book Data: ", data: book });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to Fetch movie details" });
+  }
+});
 
-async function getAllBookByTitle(bookTitle){
-    try {
-        const book = await Book.findOne({title: bookTitle})
-        return book
-    } catch (error) {
-        throw error
-    }
+async function getAllBookByTitle(bookTitle) {
+  try {
+    const book = await Book.findOne({ title: bookTitle });
+    return book;
+  } catch (error) {
+    throw error;
+  }
 }
 
-
-app.get('/books/title/:bookTitle', async (req,res) => {
-    try {
-        const book = await getAllBookByTitle(req.params.bookTitle)
-        if(book){
-            res.status(201).json({message: 'Book Data: ',data: book}) 
-        }else{
-            res.status(404).json({error: 'Movie API not found'})
-        }
-    } catch (error) {
-        res.status(500).json({error: 'Failed to Fetch movie details'})
+app.get("/books/title/:bookTitle", async (req, res) => {
+  try {
+    const book = await getAllBookByTitle(req.params.bookTitle);
+    if (book) {
+      res.status(201).json({ message: "Book Data: ", data: book });
+    } else {
+      res.status(404).json({ error: "Movie API not found" });
     }
-})
+  } catch (error) {
+    res.status(500).json({ error: "Failed to Fetch movie details" });
+  }
+});
 
 async function getAllBookByAuthor(bookAuthor) {
-    try {
-        const book = await Book.findOne({author: bookAuthor})
-        return book
-    } catch (error) {
-        throw error
-    }
+  try {
+    const book = await Book.findOne({ author: bookAuthor });
+    return book;
+  } catch (error) {
+    throw error;
+  }
 }
 
-app.get('/books/author/:bookAuthor', async (req, res) => {
-    try {
-        const book = await getAllBookByAuthor(req.params.bookAuthor)
-        if(book){
-            res.status(201).json({message: 'Book Data: ',data: book}) 
-        }else{
-            res.status(404).json({error: 'Movie API not found'})
-        }
-    } catch (error) {
-        res.status(500).json({error: 'Failed to Fetch movie details'})
+app.get("/books/author/:bookAuthor", async (req, res) => {
+  try {
+    const book = await getAllBookByAuthor(req.params.bookAuthor);
+    if (book) {
+      res.status(201).json({ message: "Book Data: ", data: book });
+    } else {
+      res.status(404).json({ error: "Book API not found" });
     }
-})
+  } catch (error) {
+    res.status(500).json({ error: "Failed to Fetch movie details" });
+  }
+});
 
+async function bookDeleteById(bookId) {
+  try {
+    const book = await Book.findByIdAndDelete(bookId)
+    return book;
+  } catch (error) {
+    throw error;
+  }
+}
 
-const PORT = 3001
+app.delete("/books/:bookId", async (req, res) => {
+  try {
+    const book = await bookDeleteById(req.params.bookId);
+    if (book) {
+      res.status(201).json({ message: "Book Data deleted successfully: ", data: book });
+    } else {
+      res.status(404).json({ error: "Book API not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to Fetch movie details" });
+  }
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log('Server is running on this', PORT)
-})
+  console.log("Server is running on this", PORT);
+});
--- FE/src/components/AllBooks.jsx
+++ FE/src/components/AllBooks.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import useFetch from "../useFetch"
 
 const AllBooksData = () => {
+    const [successMessage, setSuccessMessage] = useState('')
 
     const { data, loading } = useFetch('http://localhost:3001/books')
     console.log('data: ',data)
@@ -8,14 +10,39 @@ const AllBooksData = () => {
         <p>Loading...</p>
     }
 
+    const handleDelete = async (bookId) => {
+        try {
+            const response = await fetch(`http://localhost:3001/books/${bookId}`, {
+                method: 'DELETE',
+            })
+
+            console.log(response)
+            if(!response.ok){
+                throw new Error("Failed to delete book");
+            }
+
+            const data = await response.json()
+
+            if(data){
+                console.log('Book Data Deleted Successfully',data)
+                setSuccessMessage('Book Data Deleted Successfully')
+                window.location.reload()
+            }
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
     return (
         <>
             <h1>All Books</h1>
             <ul>
                 {data?.data.map((book) => (
-                    <li key={book._id}>{book.title}</li>
+                    <li key={book._id}>{book.title} <button onClick={() => handleDelete(book._id)}>Delete</button></li>
                 ))}
             </ul>
+                <p>{successMessage}</p>
         </>
     )
 
